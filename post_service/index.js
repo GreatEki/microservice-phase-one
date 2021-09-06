29ee136f71ceccfad0a6e664e6314c38595bd4a1
Greateki <ekeneidiagbor@gmail.com>
2021-09-06 14:58:06 +0100

    first commit

---
+++ post_service/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+// Express Middleware and Body Parser
+app.use(express.json());
+
+const posts = [];
+
+app.get('/posts', (req, res) => {
+	res.status(200).json(posts);
+});
+
+app.post('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+
+	const { title } = req.body;
+
+	posts[id] = {
+		id,
+		title,
+	};
+
+	res.status(201).send(posts[id]);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`post service listening on PORT ${PORT}`));
