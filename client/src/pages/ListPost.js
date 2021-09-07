ba47187ca401b78671ba8fbdb8fe9b7ce0a2858e
Greateki <ekeneidiagbor@gmail.com>
2021-09-07 09:53:24 +0100

    chore: fetching and rendering posts

--- client/src/pages/ListPost.js
+++ client/src/pages/ListPost.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { postServiceUrl } from 'api/urls';
 
 const ListPost = () => {
-	return <div>This is the List Post Component</div>;
+	const [posts, setPosts] = useState({});
+
+	useEffect(() => {
+		(async () => {
+			const res = await axios.get(`${postServiceUrl}/posts`);
+
+			setPosts(res.data);
+		})();
+	}, []);
+
+	const renderPosts = Object.values(posts).map((post, index) => {
+		return (
+			<div
+				key={index}
+				className='card'
+				style={{ width: '30%', marginBottom: '20px' }}>
+				<div className='card-body'>
+					<h3>{post.title}</h3>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div className='d-flex flex-row flex-wrap justify-content-between'>
+			{renderPosts}
+		</div>
+	);
 };
 
 export default ListPost;
--- post_service/index.js
+++ post_service/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const posts = [];
+const posts = {};
 
 app.get('/posts', (req, res) => {
 	res.status(200).send(posts);
