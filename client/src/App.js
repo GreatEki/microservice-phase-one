ce8d420204960741722441064bb29b8bf689d580
Greateki <ekeneidiagbor@gmail.com>
2021-09-07 09:34:54 +0100

    chore: added config for absolute imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import CreatePost from './pages/CreatePost';
+import CreatePost from 'pages/CreatePost';
+import ListPost from 'pages/ListPost';
 
 const App = () => {
 	return (
 		<div className='container'>
 			<h1> Create Post </h1>
 			<CreatePost />
+			<br />
+			<hr />
+			<h1> List Post Component</h1>
+			<ListPost />
 		</div>
 	);
 };
---
+++ client/src/pages/ListPost.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListPost = () => {
+	return <div>This is the List Post Component</div>;
+};
+
+export default ListPost;
