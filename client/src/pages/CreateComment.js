bc9e69e45dc92bb2f8993111d76fd33f09bfbb7f
Greateki <ekeneidiagbor@gmail.com>
2021-09-07 10:11:29 +0100

    feat: implemeted feat to add comment

---
+++ client/src/pages/CreateComment.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { commentsServiceUrl } from 'api/urls';
+
+const CreateComment = (props) => {
+	const { postId } = props;
+
+	const [content, setContent] = useState('');
+
+	const onSend = async (e) => {
+		e.preventDefault();
+
+		const payload = {
+			content,
+		};
+
+		const response = await axios.post(
+			`${commentsServiceUrl}/posts/${postId}/comments`,
+			payload
+		);
+
+		console.log(response);
+		setContent('');
+	};
+	return (
+		<div>
+			<form onSubmit={onSend}>
+				<div className='form=group'>
+					<label> Comment here</label>
+					<input
+						className='form-control'
+						value={content}
+						onChange={(e) => setContent(e.currentTarget.value)}
+					/>
+				</div>
+				<button className='btn btn-primary'> Send </button>
+			</form>
+		</div>
+	);
+};
+
+export default CreateComment;
--- client/src/pages/ListPost.js
+++ client/src/pages/ListPost.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { postServiceUrl } from 'api/urls';
 
+import CreateComment from 'pages/CreateComment';
+
 const ListPost = () => {
 	const [posts, setPosts] = useState({});
 
@@ -10,6 +12,7 @@ const ListPost = () => {
 			const res = await axios.get(`${postServiceUrl}/posts`);
 
 			setPosts(res.data);
+			console.log(res.data);
 		})();
 	}, []);
 
@@ -21,6 +24,7 @@ const ListPost = () => {
 				style={{ width: '30%', marginBottom: '20px' }}>
 				<div className='card-body'>
 					<h3>{post.title}</h3>
+					<CreateComment postId={post.id} />
 				</div>
 			</div>
 		);
